e0681467cae010b112497867ebaad8962abccbda
BlaBlouBlaBli <hugomiroude@gmail.com>
2025-12-17 14:02:54 +0100

    Create tic tac toe local.py

---
+++ tic tac toe local.py
@@ -0,0 +1,54 @@
+import socket
+
+HOST = "0.0.0.0" 
+PORT = 5000
+
+def grille():
+    return [[" " for _ in range(3)] for _ in range(3)]
+
+def afficher(L):
+    for ligne in L:
+        print(ligne)
+    print()
+
+def win(L, j):
+    for i in range(3):
+        if all(L[i][k] == j for k in range(3)): return True
+        if all(L[k][i] == j for k in range(3)): return True
+    if all(L[i][i] == j for i in range(3)): return True
+    if all(L[i][2-i] == j for i in range(3)): return True
+    return False
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+server.listen(1)
+
+print("En attente de connexion...")
+conn, addr = server.accept()
+print("Connecté à", addr)
+
+plateau = grille()
+
+for tour in range(9):
+    afficher(plateau)
+
+    if tour % 2 == 0:
+        n = int(input("Ton coup (1-9): "))
+        conn.send(str(n).encode())
+        joueur = "X"
+    else:
+        n = int(conn.recv(1024).decode())
+        print("Coup adverse:", n)
+        joueur = "O"
+
+    y = (n-1)//3
+    x = (n-1)%3
+    plateau[y][x] = joueur
+
+    if win(plateau, joueur):
+        afficher(plateau)
+        print("Victoire de", joueur)
+        break
+
+conn.close()
+server.close()
